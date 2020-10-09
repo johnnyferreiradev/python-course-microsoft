c95fe4e60261e84c0e83df3bddaec34ae2fbb307
johnnyferreiradev <johnnyferreiradevweb@gmail.com>
2020-10-09 10:29:22 -0300

    Hello world

---
+++ hello-world.py
@@ -0,0 +1,9 @@
+print('Hello World!')
+print("Hello World!")
+
+name = input('Please enter yout name: ')
+print(name)
+
+print()
+print('Did you see that blank line?')
+print('Blank line \nin the middle of string')
